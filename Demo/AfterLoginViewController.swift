5e746b5061628bd0da4818100a55e556bc12fc77
余宜臻 <yuyizhen@yuyizhende-MacBook-Pro.local>
2017-01-28 16:38:19 +0800

    first commit

---
+++ Demo/AfterLoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AfterLoginViewController.swift
+//  Demo
+//
+//  Created by 郭介騵 on 2016/11/29.
+//  Copyright © 2016年 james12345. All rights reserved.
+//
+
+import Foundation
---
+++ Demo/ClassA.swift
@@ -0,0 +1,9 @@
+//
+//  ClassA.swift
+//  Demo
+//
+//  Created by 郭介騵 on 2016/11/29.
+//  Copyright © 2016年 james12345. All rights reserved.
+//
+
+import Foundation
